15868b3e744195aade59e2b015da0ff9302d9734
Pedro Duarte <pedro.duarte@gympass.com>
2019-08-07 10:51:52 -0300

    update google maps lib

--- App.js
+++ App.js
@@ -9,16 +9,18 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, MapViewAnimated } from 'react-native-maps';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView provider={PROVIDER_GOOGLE} style={{ width: '100%', height: '100%' }} />
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{ width: '100%', height: '100%' }}
+          onRegionChange={(event)=>{console.log(event)}}
+        />
       </View>
     );
   }
 }
-
-
